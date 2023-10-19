acec2e51a9cd95a69ea167d7469173408930c116
s23380426 <haruzinms@yandex.ru>
2023-10-19 17:12:20 +0300

    jkj

--- 1-type/double/double.h
+++ 1-type/double/double.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 bool compare_double(double x, double y) {
     double e=0.0000000001;
     if (fabs(x-y)<e) return true;
@@ -10,5 +12,10 @@ int get_nearest_int(double x){
 }
 
 double get_fractional(double x) {
+    int y;
     return x - floor(x);
+    if (fabs(x-y)<0.00000001);
+    else return false;
+    return true;
 }
+
