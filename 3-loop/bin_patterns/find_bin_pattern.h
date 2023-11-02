d33d1d5ec3a472601931c7857aca971a700261de
Michel-23 <haruzinms@yandex.ru>
2023-11-02 17:43:10 +0300

    556

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,6 +1,16 @@
 #include <math.h>
 
-char find_bin_pattern(int number){
-    
-    return 100;
+int find_bin_pattern(int n){
+    char bin[32];
+    int find_bin_pattern=0;
+    for (int i=31;i>=0;i--){
+        bin[i]=(n & 1) + '0';
+        n>>=1;
+    }
+    for (int i=0;i<=29;i++){
+        if (bin[i]=='1' && bin[i+1]=='0' && bin[i+2]=='1'){
+            find_bin_pattern++;
+        }
+    }
+    return find_bin_pattern;
 }
--- 3-loop/count_max_digit/count_max_digit.h
+++ 3-loop/count_max_digit/count_max_digit.h
@@ -1,5 +1,20 @@
 #include <math.h>
 
 char count_max_digit(long long int){
-    return 100;
+    int x;
+    int elem;
+    int max_elem=0;
+    int c=0;
+    while (x>0){
+        elem=x%10;
+        if (elem>max_elem){
+            max_elem=elem;
+            c=1;
+        }
+        else if (elem==max_elem){
+            c++;
+        }
+        x=x/10;
+    }
+    return c;
 }
