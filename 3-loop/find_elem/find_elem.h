6de719a3034c645ffa44804a66c30eb76862a06c
Michel-23 <haruzinms@yandex.ru>
2023-11-05 00:22:25 +0300

    ggg

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -2,6 +2,18 @@
 
 int find_elem(int left, int right, int number, int max_iter){
     int i = 0; //Счётчик итераций
+    while ((right-left)!=0){
+        if (number<((right-left)/2)){
+            right=((right-left)/2);
+        }
+        else{
+            left=((right-left)/2);
+        }
+        i++;
+    }
+    if ((right-left)==0){
+        number=right;
+    }
 
 
     return i; //НЕ МЕНЯТЬ!
