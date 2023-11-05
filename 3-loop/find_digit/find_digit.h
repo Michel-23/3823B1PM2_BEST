2fe092add9a797e20c72b5facf5e5fcfd39350b8
Michel-23 <haruzinms@yandex.ru>
2023-11-05 12:57:16 +0300

    ww\

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,30 +1,5 @@
 #include <math.h>
 
 char find_digit(double number, char digit){
-    long long int ostatok = 0;
-    long long int num;
-    long long int del = number;
-    long long int lelll= number;
-    if ((999<number) && (number<10000)){
-        num=number*1000000000;
-        while (num!=0){
-        ostatok = num%10;
-        if (ostatok == digit){
-            digit=statok;
-        }
-        num=num/10;
-    }
-
-    int c=0;
-
-    while (del!=0){
-        int chislo = del%10;
-        if (chislo == max){
-            c++;
-    }
-    del = del / 10;
-    }
-
-    return c;
-    }
+    return 0;
 }
