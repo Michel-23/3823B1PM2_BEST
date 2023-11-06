b4af0edf82f8f08cfe13dca9b640d96db9feb5ba
Michel-23 <haruzinms@yandex.ru>
2023-11-06 07:16:54 +0300

    611

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -2,6 +2,6 @@
 
 int find_elem(int left, int right, int number, int max_iter){
     int i = 0; //Счётчик итераций
-    
+
     return i; //НЕ МЕНЯТЬ!
 }
--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,5 +1,13 @@
 #include <math.h>
 
 double limit(double eps, int n_max){
-    return 0.0;
+    int n=0;
+    double y1=10;
+    double y2=0;
+    while (fabs(y1-y2)>eps && n<n_max){
+        n++;
+        y1=y2;
+        y2=n/sqrt(pow(n,2)+n);
+    }
+    return y2;
 }
