a79ba0ddc3cb1939797fb3fdfdefb2c037d8c639
Michel-23 <haruzinms@yandex.ru>
2023-11-09 17:49:08 +0300

    91123

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -1,3 +1,12 @@
 int maxmin(int input[], int size){
-    return -1;
+    int max=0;
+    for (int i=1;i<size;i++){
+        if ((input[i-1]>input[i]) && (input[i]<input[i+1]) && (input[i]>max))
+            max=input[i];
+    }
+    if (input[0]<input[1] && input[0]>max)
+        max=input[0];
+    if (input[size-1]<input[size-2] && input[size-1]>max)
+        max=input[size-1];
+    return max;
 }
