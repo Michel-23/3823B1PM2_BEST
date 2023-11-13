f33f9d9acdcc1c443b34b1bd78b29370f2d24cb4
Michel-23 <haruzinms@yandex.ru>
2023-11-13 21:53:02 +0300

    13111

--- 4-array/det/determinant.h
+++ 4-array/det/determinant.h
@@ -1,5 +1,45 @@
 #include <math.h>
 
-int determinant(int matrix[], int size){
+int f(int matrix[], int size, int coif){
+    int opred = 0;
+    int coif1 = 1;
+    int count = 0;
+    int cons = sqrt(size);
+    if ((sqrt(size) - (double)cons) > 0.000000000001)
+        return -404;
+    if (cons == 2){
+        opred = matrix[0] * matrix[3] - matrix[1] * matrix[2];
+        return opred * coif;
+        }
     
+    if (cons > 2){
+        for (int k = 0; k < cons; k++){
+            int raz = (cons - 1) * (cons - 1); 
+            int now[raz];
+            int w = 0;
+            for (int i = 0; i < cons; i ++){
+                for (int j = 0; j < cons; j ++){
+                    if ((j != k) && ((i * cons + j) >= cons)){
+                        now[w] = matrix[i * cons + j];
+                        w ++;
+                        }
+                }      
+            }
+            if (k % 2 == 0)
+                coif1 = matrix[k];
+            else 
+                coif1 = (-1) * matrix[k];
+            coif1 *= coif;
+            count += f(now, raz, coif1);
+        }
+    }
+
+    return count;
+}
+
+int determinant(int matrix[], int size){
+    int rez = 0;
+    int coif = 1;
+    rez = f(matrix, size, coif);
+    return rez;
 }
--- 4-array/kernel/find_kernel.h
+++ 4-array/kernel/find_kernel.h
@@ -1,6 +1,50 @@
 #include <math.h>
 
-int find_matrix_kernel(int matrix[], int kernel[], int matrix_row /*высота*/, int matrix_col /*длина*/, int kernel_row /*высота*/, int kernel_col /*длина*/){
-    
-    return 1;
+int shislo(int mas1[], int mas2[], int a1, int b1, int a2, int b2,int a3, int b3, int index){
+    int count = 0;
+    for (int i = 0; i < b2; i ++){
+        for (int j = 0; j < a2; j ++){
+            int basem = mas1[(i + (index / b2)) * a1 + j + (index % a2)];
+            count += mas1[(i + (index / a3)) * a1 + j + (index % a3)] * mas2[i * b2 + j];
+        }
+    }
+    return count;
+}
+
+int find_matrix_kernel(int matrix[], int kernel[], int matrix_row, int matrix_col, int kernel_row, int kernel_col){
+    int dl1 = matrix_col;
+    int vis1 = matrix_row;
+    int dl2 = kernel_col;
+    int vis2 = kernel_row;
+    int raz1 = dl1 - dl2;
+    int raz2 = vis1 - vis2; 
+    int new_raz = (raz1 + 1) * (raz2 + 1);
+    int* matrix1 = matrix;
+    int flag = 1;
+    while (flag){
+        int dl3 = raz1 + 1;
+        int vis3 = raz2 + 1;
+        int* mas = (int*)malloc(sizeof(int) * new_raz);
+        for (int i = 0; i < vis3; i++){
+            for (int j = 0; j < dl3; j++){
+                int index = i * dl3 + j; 
+                mas[index] = shislo(matrix1, kernel, dl1, vis1, dl2, vis2, dl3, vis3, index);
+            }
+            
+        }
+        dl1 = dl3;
+        vis1 = vis3;
+        raz1 = dl1 - dl2;
+        raz2 = vis1 - vis2;
+        matrix1 = mas;
+        if (((raz1 + 1) >= 0) && ((raz2 + 1) >= 0)) 
+            new_raz = (raz1 + 1) * (raz2 + 1);
+        else 
+            flag = 0;
+    }
+    int otv = 0;
+    for (int k = 0; k < new_raz; k++){
+        otv += matrix1[k];
+    }
+    return otv;
 }
