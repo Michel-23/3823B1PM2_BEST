bf3a9e2aad992a4f37d71fbcbe1d9811a7768c4e
Michel-23 <haruzinms@yandex.ru>
2023-11-27 21:06:51 +0300

    nee

--- 6-functions/kramer/kramer_method.h
+++ 6-functions/kramer/kramer_method.h
@@ -1,5 +1,40 @@
+#include <stdio.h>
 #include <math.h>
 
+double delmat(int* matrix, int* vector, int size, int rasp) { // делает новые определители
+     int sq = sqrt(size); // 4->2 9->3
+     int mat[size]; // создание нового массива
+
+     for (int i = 0; i < size; i++){
+          mat[i] = matrix[i]; // присваивание начений из matrix
+     }
+
+     for (int i = 0; i < sq; i++) { // 
+          mat[rasp /*номер столбца*/+sq*i] = vector[i]; // сама замена поэлементно 
+     }
+
+     if (size == 4)
+          return mat[0] * mat[3] - mat[1] * mat[2];
+     if (size == 9)
+          return mat[0]*mat[4]*mat[8]+ mat[6]*mat[1]*mat[5] + mat[3]*mat[7]*mat[2]-mat[6]*mat[4]*mat[2]-mat[0]*mat[7]*mat[5]-mat[3]*mat[1]*mat[8];
+}
+
 double kramer_method(int* matrix, int* vector, int size){
-     return 100;
+     int del=0;
+     int sq = sqrt(size);
+
+     double x[] = {0,0,0,0};
+
+     if (size==4)
+          del = matrix[0] * matrix[3] - matrix[1] * matrix[2];
+     else
+          del = matrix[0]*matrix[4]*matrix[8]+ matrix[6]*matrix[1]*matrix[5] + matrix[3]*matrix[7]*matrix[2]-matrix[6]*matrix[4]*matrix[2]-matrix[0]*matrix[7]*matrix[5]-matrix[3]*matrix[1]*matrix[8];
+
+     for (int i = 0; i < sq; i++) {
+          x[i] = delmat(matrix, vector, size, i);
+     }
+
+     if (del != 0)
+          return (x[0]/del) + (x[1]/del) + (x[2]/del);
+     return -1;
 }
--- 6-functions/subarray/subarray_finder.h
+++ 6-functions/subarray/subarray_finder.h
@@ -1,57 +1,23 @@
 #include <math.h>
 
 int find_subarray(int* array, int array_size, int* subarray1, int subarray_size1, int* subarray2, int subarray_size2, int* subarray3, int subarray_size3){
-    int i=0;
-    for (i = 0; i<=array_size - array_size;i++){
-        int j=0;
-        for (j=0;j<array_size;j++){
-            if (array[i+j] != array[j])
-                break;
+    int a=-1;
+    int c=-1;
+    int k=-1;
+    int t=-1;
+    for (int i=0;i<array_size;i++){
+        if (array[i] == array[0] && array[i+1] == array[1]){
+            int a=i;
         }
-        if (j==array_size) 
-            return i;
-    }
-    return -1;
-}
-
-int find_subarra(int* array, int array_size, int* subarray1, int subarray_size1, int* subarray2, int subarray_size2, int* subarray3, int subarray_size3) {
-    int k = -1, p = -1, m = -1;
-
-    for (int i = 0; i <= array_size - subarray_size1; i++) {
-        int j = 0;
-        for (j = 0; j < subarray_size1; j++) {
-            if (array[i + j] != subarray1[j])
-            break;
+        if (array[i] == subarray1[0] && array[i+1] == subarray1[1]){
+            int c=i;
         }
-        if (j == subarray_size1) {
-            k = i;
-            break;
+        if (array[i] == subarray2[0] && array[i+1] == subarray2[1]){
+            int k=i;
+        }
+        if (array[i] == subarray3[0] && array[i+1] == subarray3[1]){
+            int t=i;
         }
     }
-
-    for (int i = 0; i <= array_size - subarray_size2; i++) {
-        int j = 0;
-        for (j = 0; j < subarray_size2; j++) {
-            if (array[i + j] != subarray2[j])
-            break;
-        }
-        if (j == subarray_size2) {
-            p = i;
-            break;
-        }
-    }
-
-    for (int i = 0; i <= array_size - subarray_size3; i++) {
-        int j = 0;
-        for (j = 0; j < subarray_size3; j++) {
-            if (array[i + j] != subarray3[j])
-            break;
-        }
-        if (j == subarray_size3) {
-            m = i;
-            break;
-        }
-    }
-
-    return k + p + m;
+    return a+c+k+t;
 }
