26c24acfc7d4fedd1e5720ea61f10dcb662ebb7c
Michel-23 <haruzinms@yandex.ru>
2023-12-12 23:58:24 +0300

    1212

--- 8-strings/palindrome/find_palindrom.h
+++ 8-strings/palindrome/find_palindrom.h
@@ -1,6 +1,24 @@
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
 
-int is_palindrom(char text[]){
-    return -100;
+int is_palindrom(char text[]) {
+    int length = strlen(text);
+    int i = 0;
+    int j = length - 1;
+
+    while (i < j) {
+        while (i < j && !(('a' <= text[i] && text[i] <= 'z') || ('A' <= text[i] && text[i] <= 'Z'))) {
+            i++;
+        }
+        while (i < j && !(('a' <= text[j] && text[j] <= 'z') || ('A' <= text[j] && text[j] <= 'Z'))) {
+            j--;
+        }
+        if (text[i] != text[j] && (text[i] - 32 != text[j] && text[i] + 32 != text[j])) {
+            return 0;
+        }
+        i++;
+        j--;
+    }
+    return 1;
 }
--- 8-strings/str_to_double/str_to_double_convert.h
+++ 8-strings/str_to_double/str_to_double_convert.h
@@ -1,6 +1,31 @@
 #include <math.h>
 #include <string.h>
 
-double convert(char num[]){
-    return 0.0;
+double convert(char num[]) {
+    double result = 0.0; // будет содержать итоговое значение числа
+    int nomer = 0; // сколько десятичных знаков обработано
+    int znak = 1; // знак числа
+    int i = 0; // для перебора символов
+
+    if (num[0] == '-') { // если первый символ - то знак -
+        znak = -1;
+        i++; // идем дальше
+    }
+
+    for (i; num[i] != '\0'; i++) { // перебираем до последнего
+        if (num[i] == '.') {
+            nomer = 1; // чтобы начать обрабатывать дробную часть числа
+        }
+        else {
+            int cifra = num[i] - '0'; // символ интерпретируется как цифра
+            if (nomer) {
+                result = result + (double)cifra / pow(10, nomer); // представление вещественной части числа (после точки)
+                nomer++;
+            }
+            else {
+                result = result * 10 + cifra; // типичное составление целочисленного числа
+            }
+        }
+    }
+    return result * znak;
 }
